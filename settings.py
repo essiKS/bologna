32eda891602110b3800f4485d996ff008a282762
Essi <essi.kujansuu@eui.eu>
2018-01-17 11:32:32 +0100

    D-AT

--- settings.py
+++ settings.py
@@ -156,9 +156,9 @@ SESSION_CONFIGS = [
         'num_demo_participants': 12,
         'treatment': 'all_taxes',
         # the allowed treatments are 'no_taxes', 'worker_tax', 'employer_tax', and 'all_taxes'
-        'timeline': 'wage',
+        'timeline': 'direct',
         # allowed timelines are 'direct' and 'wage' for the to applications to sequence, directauction and wageauction
-        'app_sequence': ['italiantutorial', 'italianwage', 'endsurvey'],
+        'app_sequence': ['italiantutorial', 'italiandirect', 'endsurvey'],
     },
 
 ]
